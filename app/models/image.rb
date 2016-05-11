aabbfc1d8a3636a21d2087b346af06b53731dadf
hashmat MH <hashmat00@aol.com>
2016-05-11 01:06:57 +0000

    initial update

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,14 +2,17 @@ class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
   validate :picture_size
-  
-  
+
   private
   
   def picture_size
-    if picture_size > 5.megabytes
-      errors.add(:picture, "should be less than 5mb")
+  
+    if picture.size > 5.megabytes
+    
+    errors.add(:picture, "should be less than 5MB")
+    
     end
+  
   end
   
   
